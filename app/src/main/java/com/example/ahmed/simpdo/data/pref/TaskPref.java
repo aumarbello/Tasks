51c2b113736d2ef5eb8caa525b82c05ed9266d4d
Ahmed Umar <khattabu@gmail.com>
2017-09-05 17:23:00 +0100

    Added Preference class

---
+++ app/src/main/java/com/example/ahmed/simpdo/data/pref/TaskPref.java
@@ -0,0 +1,54 @@
+package com.example.ahmed.simpdo.data.pref;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.preference.PreferenceManager;
+
+import com.example.ahmed.simpdo.R;
+
+import javax.inject.Inject;
+
+/**
+ * Created by ahmed on 9/5/17.
+ */
+
+public class TaskPref {
+    private SharedPreferences sharedPreferences;
+    private Resources resources;
+
+    @Inject
+    public TaskPref(Context context){
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        resources = context.getResources();
+    }
+
+    public boolean getPreviousTasks(){
+        return sharedPreferences.getBoolean(resources.getString
+                (R.string.key_show_previous_tasks), true);
+    }
+
+    public int getKeepDays(){
+        String dayString = sharedPreferences.getString(resources.getString
+                (R.string.number_of_days_key), "3");
+        return Integer.parseInt(dayString);
+    }
+
+    public String getNotificationCategory(){
+        return sharedPreferences.getString(resources.getString
+                (R.string.show_notification_key), "Important");
+    }
+
+    public boolean isDaysColorRandom(){
+        return sharedPreferences.getBoolean(resources.getString
+                (R.string.days_color_key), true);
+    }
+
+    //todo color for each day header
+
+    public int getDoneTaskColor(){
+        return sharedPreferences.getInt(resources.getString
+                (R.string.done_select_color_key), Color.GRAY);
+    }
+}
